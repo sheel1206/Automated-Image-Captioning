4465fa3dcafaa167c6209a0d3b552e877249f0cb
sheel1206 <sheelpatel2001@gmail.com>
2024-12-18 21:13:55 -0500

    Test Files added

---
+++ inference.py
@@ -0,0 +1,178 @@
+from gtts import gTTS
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader, Dataset, Subset
+from torchvision import transforms
+from transformers import AutoTokenizer, AutoModelForCausalLM, get_scheduler, CLIPProcessor, CLIPModel
+import pandas as pd
+import os
+from PIL import Image
+import numpy as np
+import nltk
+from sklearn.model_selection import train_test_split
+import pandas as pd
+from typing import Tuple, List, Union, Optional
+from torch.optim import AdamW
+from tqdm import tqdm, trange
+import clip
+import torch.nn.functional as nnf
+
+N = type(None)
+V = np.array
+ARRAY = np.ndarray
+ARRAYS = Union[Tuple[ARRAY, ...], List[ARRAY]]
+VS = Union[Tuple[V, ...], List[V]]
+VN = Union[V, N]
+VNS = Union[VS, N]
+T = torch.Tensor
+TS = Union[Tuple[T, ...], List[T]]
+TN = Optional[T]
+TNS = Union[Tuple[TN, ...], List[TN]]
+TSN = Optional[TS]
+TA = Union[T, ARRAY]
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+clip_model = CLIPModel.from_pretrained("openai/clip-vit-base-patch32").to(device)
+clip_processor = CLIPProcessor.from_pretrained("openai/clip-vit-base-patch32")
+gpt_tokenizer = AutoTokenizer.from_pretrained('gpt2')
+
+class MLP(nn.Module):
+
+    def forward(self, x: T) -> T:
+        return self.model(x)
+
+    def __init__(self, sizes: Tuple[int, ...], bias=True, act=nn.Tanh):
+        super(MLP, self).__init__()
+        layers = []
+        for i in range(len(sizes) -1):
+            layers.append(nn.Linear(sizes[i], sizes[i + 1], bias=bias))
+            if i < len(sizes) - 2:
+                layers.append(act())
+        self.model = nn.Sequential(*layers)
+
+
+class ClipCaptionModel(nn.Module):
+
+    #@functools.lru_cache #FIXME
+    def get_dummy_token(self, batch_size: int, device: device) -> T:
+        return torch.zeros(batch_size, self.prefix_length, dtype=torch.int64, device=device)
+
+    def forward(self, tokens: T, prefix: T, mask: Optional[T] = None, labels: Optional[T] = None):
+        embedding_text = self.gpt.transformer.wte(tokens)
+        prefix_projections = self.clip_project(prefix).view(-1, self.prefix_length, self.gpt_embedding_size)
+        #print(embedding_text.size()) #torch.Size([5, 67, 768])
+        #print(prefix_projections.size()) #torch.Size([5, 1, 768])
+        embedding_cat = torch.cat((prefix_projections, embedding_text), dim=1)
+        if labels is not None:
+            dummy_token = self.get_dummy_token(tokens.shape[0], tokens.device)
+            labels = torch.cat((dummy_token, tokens), dim=1)
+        out = self.gpt(inputs_embeds=embedding_cat, labels=labels, attention_mask=mask)
+        return out
+
+    def __init__(self, prefix_length: int, prefix_size: int = 512):
+        super(ClipCaptionModel, self).__init__()
+        self.prefix_length = prefix_length
+        self.gpt = AutoModelForCausalLM.from_pretrained('gpt2').to(device)
+        self.gpt_embedding_size = self.gpt.transformer.wte.weight.shape[1]
+        if prefix_length > 10:  # not enough memory
+            self.clip_project = nn.Linear(prefix_size, self.gpt_embedding_size * prefix_length)
+        else:
+            self.clip_project = MLP((prefix_size, (self.gpt_embedding_size * prefix_length) // 2, self.gpt_embedding_size * prefix_length))
+
+
+class ClipCaptionPrefix(ClipCaptionModel):
+
+    def parameters(self, recurse: bool = True):
+        return self.clip_project.parameters()
+
+    def train(self, mode: bool = True):
+        super(ClipCaptionPrefix, self).train(mode)
+        self.gpt.eval()
+        return self
+
+def generate(model,tokenizer, tokens=None, prompt=None, embed=None, entry_count=1,
+            entry_length=67,  # maximum number of words
+            top_p=0.8, temperature=1., stop_token: str = '.',):
+    model.eval()
+    generated_num = 0
+    generated_list = []
+    stop_token_index = tokenizer.encode(stop_token)[0]
+    filter_value = -float("Inf")
+    device = next(model.parameters()).device
+
+    with torch.no_grad():
+        for entry_idx in trange(entry_count):
+            if embed is not None:
+                generated = embed
+            else:
+                if tokens is None:
+                    tokens = torch.tensor(tokenizer.encode(prompt))
+                    tokens = tokens.unsqueeze(0).to(device)
+
+                generated = model.gpt.transformer.wte(tokens)
+
+            for i in range(entry_length):
+
+                outputs = model.gpt(inputs_embeds=generated)
+                logits = outputs.logits
+                logits = logits[:, -1, :] / (temperature if temperature > 0 else 1.0)
+                sorted_logits, sorted_indices = torch.sort(logits, descending=True)
+                cumulative_probs = torch.cumsum(nnf.softmax(sorted_logits, dim=-1), dim=-1)
+                sorted_indices_to_remove = cumulative_probs > top_p
+                sorted_indices_to_remove[..., 1:] = sorted_indices_to_remove[
+                                                    ..., :-1
+                                                    ].clone()
+                sorted_indices_to_remove[..., 0] = 0
+
+                indices_to_remove = sorted_indices[sorted_indices_to_remove]
+                logits[:, indices_to_remove] = filter_value
+                next_token = torch.argmax(logits, -1).unsqueeze(0)
+                next_token_embed = model.gpt.transformer.wte(next_token)
+                if tokens is None:
+                    tokens = next_token
+                else:
+                    tokens = torch.cat((tokens, next_token), dim=1)
+                generated = torch.cat((generated, next_token_embed), dim=1)
+                if stop_token_index == next_token.item():
+                    break
+
+            output_list = list(tokens.squeeze().cpu().numpy())
+            output_text = tokenizer.decode(output_list)
+            generated_list.append(output_text)
+
+    return generated_list[0]
+
+def generate_caption(model, tokenizer, image_tensor):
+    with torch.no_grad():
+        prefix = clip_model.get_image_features(**image_tensor).to(device, dtype=torch.float32)
+        prefix_embed = model.clip_project(prefix).reshape(1, prefix_length, -1)
+
+
+        generated_text_prefix = generate(
+            model=model,
+            tokenizer= tokenizer,
+            embed=prefix_embed,
+            entry_count=1,
+            entry_length=67,  # Max words
+            top_p=0.8,  # Sampling parameter
+            stop_token=".",
+        )
+    generated_text_prefix = generated_text_prefix.replace("<|endoftext|>", "").strip()
+    return generated_text_prefix
+
+
+prefix_length = 10
+model_path = "clip_caption_model2.pth"
+
+model = ClipCaptionModel(prefix_length=prefix_length).to(device)
+model.load_state_dict(torch.load(model_path, map_location=device))
+# Load and preprocess the image
+image_path = "./test.jpg"  # Replace with your image path
+image = Image.open(image_path).convert('RGB')
+
+image_tensor = clip_processor(images = image, return_tensors='pt').to(device)
+captions = generate_caption(model, gpt_tokenizer, image_tensor)
+print("Generated Cation:", captions)
+tts = gTTS(text=captions, lang='en')
+tts.save("caption_audio.mp3")  
